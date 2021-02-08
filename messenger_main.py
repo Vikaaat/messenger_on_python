f11c99dcaca18d88409e180fa3a16a3acde255c3
Vika <47684687+Vikaaat@users.noreply.github.com>
2021-02-08 22:56:18 +0400

    Update messenger_main.py

--- messenger_main.py
+++ messenger_main.py
@@ -1,23 +1,32 @@
 import datetime
 import time
 
-db = [] # database
+db = [] # database of messages
 # send_message == put message into db
 # get_message == output db's messages
 def send_message(name, text):
     # TODO: make message
     # TODO: add to db
     message = {
-        'time':time.time(),
+        'time': time.time(),
         'name': name,
         'text': text
                 }
     db.append(message)
 
-def get_message():
-    return
+def get_message(after):
+    result = []
+    for message in db:
+        if message['time'] > after:
+            result.append(message)
+    return result
 
 send_message('123','123')
 send_message('123','6776')
 send_message('123','145')
-print(db)
+t1 = db[-1]['time']
+print(get_message(t1))
+
+send_message('123','123')
+send_message('123','6776')
+print(get_message(t1))
